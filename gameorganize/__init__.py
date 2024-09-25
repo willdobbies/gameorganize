26358468d0bbd59c75c7a03691942412698499d2
Chris <ccriddler@protonmail.com>
2024-09-24 18:53:24 -0700

    Switched to flask application factory structure

--- gameorganize/__init__.py
+++ gameorganize/__init__.py
@@ -1,1 +1,31 @@
-from gameorganize.app import app
+from flask import Flask
+from .db import db
+from flask_migrate import Migrate
+
+def create_app():
+    from .config import DevelopmentConfig
+
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+
+    migrate = Migrate(app, db)
+
+    register_blueprints(app)
+    init_extensions(app)
+
+    return app
+
+def register_blueprints(app):
+    from .game import game
+    from .gamelist import gamelist
+    from .importer import importer
+    from .platform import platform
+
+    app.register_blueprint(game, url_prefix='/game')
+    app.register_blueprint(gamelist)
+    app.register_blueprint(importer, url_prefix='/import')
+    app.register_blueprint(platform, url_prefix='/platform')
+
+def init_extensions(app):
+    from .db import db
+    db.init_app(app)
--- gameorganize/app.py
+++ gameorganize/app.py
@@ -1,27 +1,6 @@
-from .db import db
-from .game import game
-from .gamelist import gamelist
-from .importer import importer
-from .platform import platform
-from flask import Flask
-from flask_migrate import Migrate
-from .config import DevelopmentConfig
+from . import create_app
 
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+app = create_app()
 
-# Add blueprint modules
-app.register_blueprint(game, url_prefix='/game')
-app.register_blueprint(gamelist)
-app.register_blueprint(importer, url_prefix='/import')
-app.register_blueprint(platform, url_prefix='/platform')
-
-# setup sqlalchemy
-db.init_app(app)
-
-# setup migrations
-migrate = Migrate(app, db)
-
-# Create the database tables and start app
-with app.app_context():
-  db.create_all()
+if __name__ == '__main__':
+  app.run()
