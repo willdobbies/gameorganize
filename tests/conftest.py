d591d545da43fa118f72470f8580b4764c8a4dd4
Chris <cjriddle31@gmail.com>
2025-05-20 11:32:50 -0700

    reduce code duplication in game tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,11 @@ def pytest_addoption(parser):
     parser.addoption("--apiId", action="store")
     parser.addoption("--apiKey", action="store")
 
-@pytest.fixture
+@pytest.fixture()
 def apiId(request):
     return request.config.getoption("--apiId")
 
-@pytest.fixture
+@pytest.fixture()
 def apiKey(request):
     return request.config.getoption("--apiKey")
 
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,70 +1,51 @@
 from gameorganize.model.game import GameEntry, Completion, Priority, Ownership
 from gameorganize.model.platform import Platform
 from gameorganize.model.user import User
+import pytest
 
-def test_game_entry(db_session):
-    # make some entries
-    valid_user = User(
-        id = 0,
+def setup_db(db_session):
+    new_user = User(
         username="user",
         password="password",
     )
-    valid_platform = Platform(
-        id = 0,
-        name="Playstation",
-        user_id = valid_user.id,
-    )
-    valid_game_entry = GameEntry(
-        name="Spyro The Dragon",
-        user_id = valid_user.id,
-        platform_id = valid_platform.id,
-    )
-
-    db_session.add(valid_user)
-    db_session.add(valid_platform)
-    db_session.add(valid_game_entry)
+    db_session.add(new_user)
     db_session.commit()
 
+    new_platform = Platform(
+        name="Playstation",
+        user_id = new_user.id,
+    )
+    db_session.add(new_platform)
+    db_session.commit()
+
+    new_game_entry = GameEntry(
+        name="Spyro The Dragon",
+        user_id = new_user.id,
+        platform_id = new_platform.id,
+    )
+    db_session.add(new_game_entry)
+    db_session.commit()
+
+def test_game_entry(db_session):
+    setup_db(db_session)
+
     search_user = db_session.query(User).filter_by(username="user").first()
-    assert (search_user.username == "user")
     assert (search_user.password == "password")
 
     search_platform = db_session.query(Platform).filter_by(name="Playstation").first()
-    assert (search_platform.user_id == valid_user.id)
+    assert (search_platform.user_id == search_user.id)
 
-    search_game = db_session.query(GameEntry).filter_by(user_id=valid_user.id).first()
+    search_game = db_session.query(GameEntry).filter_by(name="Spyro The Dragon").first()
+    assert (search_game.user_id == search_user.id)
     assert (search_game.platform_id == search_platform.id)
 
 def test_delete_platform(db_session):
-    # make some entries
-    valid_user = User(
-        id = 0,
-        username="user",
-        password="password",
-    )
-    valid_platform = Platform(
-        id = 0,
-        name="Playstation",
-        user_id = valid_user.id,
-    )
-    valid_game_entry = GameEntry(
-        name="Spyro The Dragon",
-        user_id = valid_user.id,
-        platform_id = valid_platform.id,
-    )
-
-    db_session.add(valid_user)
-    db_session.add(valid_platform)
-    db_session.add(valid_game_entry)
-    db_session.commit()
+    setup_db(db_session)
 
     search_platform = db_session.query(Platform).filter_by(name="Playstation").first()
-    assert (search_platform.user_id == valid_user.id)
+    search_game = db_session.query(GameEntry).filter_by(platform_id=search_platform.id).first()
 
-    search_game = db_session.query(GameEntry).filter_by(user_id=valid_user.id).first()
-    assert (search_game.platform_id == valid_platform.id)
-
-    db_session.delete(valid_platform)
+    db_session.delete(search_platform)
     db_session.commit()
 
     assert (search_game.platform_id == None)
