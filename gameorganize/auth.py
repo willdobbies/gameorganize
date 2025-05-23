6b61ddb506e6a24dd8ed6f54f3a48397c246db84
Chris <cjriddle31@gmail.com>
2025-05-23 10:59:21 -0700

    Combine login/signup pages on homepage. Add some basic signup security

--- gameorganize/auth.py
+++ gameorganize/auth.py
@@ -3,8 +3,11 @@ from flask_login import login_user, logout_user, login_required, current_user
 from gameorganize.db import db
 from gameorganize.model.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
 
 auth = Blueprint('auth', __name__)
+
+rex_password = re.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$")
 
 @auth.route('/login', methods=['POST'])
 def login_post():
@@ -25,9 +28,9 @@ def login_post():
     login_user(user, remember=remember)
     return redirect(url_for('user.detail', username=current_user.username))
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
+#@auth.route('/login')
+#def login():
+#    return render_template('auth/login.html')
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
@@ -36,10 +39,18 @@ def signup_post():
 
     # check if username unique
     user = User.query.filter_by(username=username).first()
+    
+    if(not username):
+        flash(f"Username must not be empty")
+        return redirect(url_for('home'))
+    
+    if(not rex_password.match(password)):
+        flash(f"Password must be at least 8 characters long and contain numbers and uppercase letters")
+        return redirect(url_for('home'))
 
     if(user):
         flash(f"User {user.username} already exists")
-        return redirect(url_for('auth.signup'))
+        return redirect(url_for('home'))
     
     # make new user!
     new_user = User(
@@ -52,14 +63,14 @@ def signup_post():
     
     # send to login page
     flash(f"User {new_user.username} created. You can now log in.")
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('home'))
 
-@auth.route('/signup')
-def signup():
-    return render_template('auth/signup.html')
+#@auth.route('/signup')
+#def signup():
+#    return render_template('auth/signup.html')
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('home'))
