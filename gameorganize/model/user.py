65afba47ad26ee921f1c8985b74f11a39b0e91c8
willdobbies <cjriddle31@gmail.com>
2025-05-24 17:50:24 -0700

    Cover potential db error where game / platform model modules are never referenced

--- gameorganize/model/user.py
+++ gameorganize/model/user.py
@@ -1,6 +1,8 @@
 from flask_login import UserMixin
 from gameorganize.db import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .game import GameEntry
+from .platform import Platform
 
 class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
